f26deb0487155e5c6be14b6be98d78d340366d1e
Scribler <steven.mc2225@gmail.com>
2021-09-22 11:03:24 -0700

    update

---
+++ classprojects/000Testing/js/script.js
@@ -0,0 +1,27 @@
+console.log('hello world');
+
+var testnum = 1;
+
+// var numlist = function(num){
+  // if(num < 6){
+    // num++;
+    // console.log(num);
+    // numlist(num);
+  // } else {
+    // console.log("You have reached", num, ".")
+  // }
+// }
+
+
+function numlist(num) {
+  if(num < 6){
+    num++;
+    console.log(num);
+    numlist(num);
+  } else {
+    console.log("You have reached", num, ".")
+  }
+}
+
+
+numlist(testnum);
